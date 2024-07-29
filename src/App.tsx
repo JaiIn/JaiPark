83be02cb62e18cf5b5778a759ba130c028ec1873
JaiIn <di0308@naver.com>
2024-07-29 21:00:06 +0900

    초기설정

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function App() {
+  return (
+    <Container>
+      
+    </Container>
+  );
+}
+
+export default App;
+
+const Container = styled.div`
+max-width: 1440px;
+`
